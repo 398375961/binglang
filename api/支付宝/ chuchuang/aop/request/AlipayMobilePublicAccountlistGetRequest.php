eeffbb7e2d08f84b438e562827bc660a8d14a687
398375961 <398375961@qq.com>
2018-11-01 13:23:39 +0800

    Add files via upload

---
+++ api/支付宝/ chuchuang/aop/request/AlipayMobilePublicAccountlistGetRequest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * ALIPAY API: alipay.mobile.public.accountlist.get request
+ *
+ * @author auto create
+ * @since 1.0, 2014-10-31 17:54:42
+ */
+class AlipayMobilePublicAccountlistGetRequest
+{
+	/** 
+	 * 分页查询传入分页参数 pagesize最大设置为200
+	 **/
+	private $bizContent;
+
+	private $apiParas = array();
+	private $terminalType;
+	private $terminalInfo;
+	private $prodCode;
+	private $apiVersion="1.0";
+	
+	public function setBizContent($bizContent)
+	{
+		$this->bizContent = $bizContent;
+		$this->apiParas["biz_content"] = $bizContent;
+	}
+
+	public function getBizContent()
+	{
+		return $this->bizContent;
+	}
+
+	public function getApiMethodName()
+	{
+		return "alipay.mobile.public.accountlist.get";
+	}
+
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+
+	public function getTerminalType()
+	{
+		return $this->terminalType;
+	}
+
+	public function setTerminalType($terminalType)
+	{
+		$this->terminalType = $terminalType;
+	}
+
+	public function getTerminalInfo()
+	{
+		return $this->terminalInfo;
+	}
+
+	public function setTerminalInfo($terminalInfo)
+	{
+		$this->terminalInfo = $terminalInfo;
+	}
+
+	public function getProdCode()
+	{
+		return $this->prodCode;
+	}
+
+	public function setProdCode($prodCode)
+	{
+		$this->prodCode = $prodCode;
+	}
+
+	public function setApiVersion($apiVersion)
+	{
+		$this->apiVersion=$apiVersion;
+	}
+
+	public function getApiVersion()
+	{
+		return $this->apiVersion;
+	}
+
+}
---
+++ api/支付宝/ chuchuang/aop/request/AlipayMobilePublicAuthGettokenRequest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * ALIPAY API: alipay.mobile.public.auth.gettoken request
+ *
+ * @author auto create
+ * @since 1.0, 2015-03-26 14:48:48
+ */
+class AlipayMobilePublicAuthGettokenRequest
+{
+	/** 
+	 * userId	 	需要授权的用户id
+thirdAppId	 	需要授权的商户id
+	 **/
+	private $bizContent;
+
+	private $apiParas = array();
+	private $terminalType;
+	private $terminalInfo;
+	private $prodCode;
+	private $apiVersion="1.0";
+	
+	public function setBizContent($bizContent)
+	{
+		$this->bizContent = $bizContent;
+		$this->apiParas["biz_content"] = $bizContent;
+	}
+
+	public function getBizContent()
+	{
+		return $this->bizContent;
+	}
+
+	public function getApiMethodName()
+	{
+		return "alipay.mobile.public.auth.gettoken";
+	}
+
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+
+	public function getTerminalType()
+	{
+		return $this->terminalType;
+	}
+
+	public function setTerminalType($terminalType)
+	{
+		$this->terminalType = $terminalType;
+	}
+
+	public function getTerminalInfo()
+	{
+		return $this->terminalInfo;
+	}
+
+	public function setTerminalInfo($terminalInfo)
+	{
+		$this->terminalInfo = $terminalInfo;
+	}
+
+	public function getProdCode()
+	{
+		return $this->prodCode;
+	}
+
+	public function setProdCode($prodCode)
+	{
+		$this->prodCode = $prodCode;
+	}
+
+	public function setApiVersion($apiVersion)
+	{
+		$this->apiVersion=$apiVersion;
+	}
+
+	public function getApiVersion()
+	{
+		return $this->apiVersion;
+	}
+
+}
